1fc362c62b64ffb1204cdb8c10745346dbff47db
Mukul Jain <mukulj@mindfiresolutions.com>
2017-02-07 23:25:27 +0530

    - Initial Commit - Preliminary UI of the app with Storyboard.

---
+++ Vibration Feedbacks/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  Vibration Feedbacks
+//
+//  Created by Mukul Jain
+//  Copyright © 2017 jain_mj. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController
+{
+    // MARK:- VC Life cycle methods
+    
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+        
+        // Get the current Device Name
+        //print ("The current device is:- \(UIDevice.current.modelName)")
+    }
+    
+    override func didReceiveMemoryWarning()
+    {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    }
+
